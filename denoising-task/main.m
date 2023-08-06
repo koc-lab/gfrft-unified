0b861b7442ae8f560f8d0cd91ea9d4e462c6b2e9
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-06 23:19:15 +0300

    feat(denoising): initialize task
    Initialize denoising-task for GFRFT experiments.

---
+++ denoising-task/main.m
@@ -0,0 +1,16 @@
+%% Clear
+clc, clear, close all;
+
+%% Initialize paths and GSPBox
+init();
+
+%% Graph Generation
+N = 4;
+A = diag(ones(N-1, 1), -1); A(1, end) = 1;
+A = A / sqrt(N);
+
+%% GFT and GFRFT
+fractionalOrder = 0.5;
+[gftMatrix, igftMatrix, graphFreqs] = gftmtx(A, 'ascend02pi', 'eig', 10);
+[gfrftMatrix, igfrftMatrix] = gfrftmtx(gftMatrix, fractionalOrder);
+

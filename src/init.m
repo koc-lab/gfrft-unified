b9ba6bb538720d1740f6f309922bc9bf95a7c005
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-06 23:04:30 +0300

    feat: implement `GFT` and `mpower` based `GFRFT`
    Implement graph Fourier transform (GFT) and `mpower()`
    based implementation of `GFRFT`.

---
+++ src/init.m
@@ -0,0 +1,13 @@
+function init(prestring)
+    arguments
+        prestring (1,1) string = ""
+    end
+
+    GSPBOX_PATH = append(prestring, "../gspbox/");
+    DATA_PATH   = append(prestring, "../tv-graph-datasets/");
+    GFT_PATH    = append(prestring, "./gft/");
+
+    addpath(DATA_PATH, GSPBOX_PATH, '-frozen');
+    addpath(GFT_PATH, '-begin');
+    gsp_start;
+end

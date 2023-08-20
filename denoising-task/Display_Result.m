336ead2b0090d8c58d21bbc821e84042fa469fd7
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-20 20:42:52 +0300

    fix: result display legends
    Only display the necessary legends.

--- denoising-task/Display_Result.m
+++ denoising-task/Display_Result.m
@@ -14,19 +14,36 @@ fractional_orders = results.fractional_orders;
 snr_dbs = results.snr_dbs;
 
 %% Figure
+axs = [];
 figure;
-legends = cell(2 * length(snr_dbs), 1);
 for i = 1:length(snr_dbs)
     [~, max_zero_count_idx] = Matrix_Idx(squeeze(estimation_snr(:, i, :)), 'max');
     curr_snr = estimation_snr(:, i, max_zero_count_idx);
-    plot(fractional_orders, curr_snr, 'LineWidth', 2);
+    max_idx = Matrix_Idx(curr_snr, 'max');
+
+    ax = plot(fractional_orders, curr_snr, 'LineWidth', 2, ...
+            'DisplayName',sprintf("Estimation for SNR = %.2f", noisy_snr(i))); 
+    axs(end + 1) = ax;
     hold on;
-    legends{2 * i - 1} = sprintf("Estimation for SNR = %.2f", noisy_snr(i));
     yline(noisy_snr(i));
-    legends{2 * i} = sprintf("Noisy SNR = %.2f", noisy_snr(i));
+
+    max_point = plot(fractional_orders(max_idx), curr_snr(max_idx), '.');
+    datatip(max_point, 'Location', 'southeast');
+
+    labels  = ["$a$", "SNR"];
+    formats = ["%.2f", "%.2f"];
+    plt = {ax, max_point};
+
+    for j = 1:length(plt)
+        for i = 1:length(plt{j}.DataTipTemplate.DataTipRows)
+            plt{j}.DataTipTemplate.DataTipRows(i).Label  = labels(i);
+            plt{j}.DataTipTemplate.DataTipRows(i).Format = formats(i);
+        end
+        plt{j}.DataTipTemplate.Interpreter = "latex";
+    end
 end
 title(sprintf("%s", dataset_title));
+legend(axs);
 grid on;
-legend(legends, 'Interpreter', 'latex');
 xlabel("Fractional Order $a$", 'Interpreter', 'latex');
 ylabel("SNR (dB)", 'Interpreter', 'latex');

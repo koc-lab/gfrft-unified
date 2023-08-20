19a99c5c53bcf22ad30a834f35654fe8631cb133
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-20 15:12:46 +0300

    feat: convert to `snr` based generation
    Convert to SNR based generation to be able to consistent and comparable
    noise generation for all input data types.
    
    Resolves: #1

--- denoising-task/Main.m
+++ denoising-task/Main.m
@@ -8,7 +8,7 @@ close all;
 %% Graph Generation
 dataset = "../data/tv-graph-datasets/pm25-concentration.mat";
 dataset_title = "PM-2.5";
-knn_count = 5;
+knn_count = 2;
 knn_sigma = 1;
 max_node_count = 100;
 max_time_instance = 100;
@@ -21,7 +21,8 @@ shift_mtx_strategy = 'adjacency';
 
 if strcmp(shift_mtx_strategy, 'laplacian')
     disp("Using Laplacian matrix and ascending ordered graph frequency.");
-    shift_mtx = eye(size(graph.W)) - full(graph.W);
+    full_adj_mtx = full(graph.W);
+    shift_mtx = diag(sum(full_adj_mtx, 2)) - full_adj_mtx;
     [gft_mtx, igft_mtx, graph_freqs] = GFT_Mtx(shift_mtx, 'ascend');
 else
     disp("Using weighted adjacency matrix and TV ordered graph frequency.");
@@ -30,12 +31,12 @@ else
 end
 
 %% Experiment
-sigmas = [0.05, 0.10];
-fractional_orders = 0.5:0.05:1.5;
+snr_dbs = [4, 5, 6, 7];
+fractional_orders = 0.0:0.01:2.0;
 zero_counts = 1:size(signals, 1) - 1;
 
-[estimation_error, noise_error] = Experiment(signals, gft_mtx, ...
-                                             fractional_orders, sigmas, zero_counts);
+[estimation_snr, noisy_snr] = Experiment(signals, gft_mtx, ...
+                                         fractional_orders, snr_dbs, zero_counts);
 
 results = struct();
 results.dataset_title = dataset_title;
@@ -45,51 +46,52 @@ results.knn_sigma = knn_sigma;
 results.max_node_count = max_node_count;
 results.max_time_instance = max_time_instance;
 results.fractional_orders = fractional_orders;
-results.sigmas = sigmas;
+results.snr_dbs = snr_dbs;
 results.zero_counts = zero_counts;
-results.estimation_error = estimation_error;
-results.noise_error = noise_error;
+results.estimation_snr = estimation_snr;
+results.noisy_snr = noisy_snr;
 
 save(sprintf("results.mat"), "-struct", "results");
 
 %% Functions
-function [estimation_error, noise_error] = Experiment(signals, gft_mtx, ...
-                                                      fractional_orders, sigmas, zero_counts)
+function [estimation_snr, noisy_snr] = Experiment(signals, gft_mtx, ...
+                                                  fractional_orders, snr_dbs, zero_counts)
     arguments
         signals(:, :) double
         gft_mtx(:, :) double {Must_Be_Square_Matrix}
         fractional_orders(:, 1) double {mustBeReal}
-        sigmas(:, 1) double {mustBeReal, mustBePositive}
+        snr_dbs(:, 1) double {mustBeReal}
         zero_counts(:, 1) double {mustBeReal, mustBePositive}
     end
 
-    estimation_error = zeros(length(fractional_orders), length(sigmas), length(zero_counts));
-    noise_error = zeros(length(sigmas), 1);
-    noisy_signals_cell = cell(1, length(sigmas));
-    for i = 1:length(sigmas)
-        noisy_signals_cell{i} = Add_Noise(signals, sigmas(i));
-        noise_error(i) = Get_MSE_Percent(signals, noisy_signals_cell{i});
+    estimation_snr = zeros(length(fractional_orders), length(snr_dbs), length(zero_counts));
+    noisy_snr = zeros(length(snr_dbs), 1);
+    noisy_signals_cell = cell(1, length(snr_dbs));
+    for i = 1:length(snr_dbs)
+        noisy_signals_cell{i} = Add_Noise(signals, snr_dbs(i));
+        noisy_snr(i) = Snr(signals, noisy_signals_cell{i});
     end
 
     for i_frac = 1:length(fractional_orders)
         [gfrft_mtx, igfrft_mtx] = GFRFT_Mtx(gft_mtx, fractional_orders(i_frac));
 
-        for j_sigma = 1:length(sigmas)
-            noisy_signals = noisy_signals_cell{j_sigma};
+        for j_snr = 1:length(snr_dbs)
+            noisy_signals = noisy_signals_cell{j_snr};
 
             for k_zero = 1:length(zero_counts)
                 filter_vec = Get_Ideal_Lowpass_Filter(size(signals, 1), zero_counts(k_zero));
                 filtered_signals = igfrft_mtx * ((gfrft_mtx * noisy_signals) .* filter_vec);
-                err = Get_MSE_Percent(signals, filtered_signals);
-                estimation_error(i_frac, j_sigma, k_zero) = err;
+                curr_snr = Snr(signals, filtered_signals);
+                estimation_snr(i_frac, j_snr, k_zero) = curr_snr;
             end
         end
     end
 end
 
-function noisy = Add_Noise(signal, sigma)
+function noisy = Add_Noise(signal, snr_db)
     rng('default');
-    noisy = signal + sigma * randn(size(signal));
+    noise = randn(size(signal)) .* std(signal) / db2mag(snr_db);
+    noisy = signal + noise;
 end
 
 function filter_vec = Get_Ideal_Lowpass_Filter(node_count, zero_count)
@@ -101,13 +103,4 @@ function filter_vec = Get_Ideal_Lowpass_Filter(node_count, zero_count)
     filter_vec = zeros(node_count, 1);
     one_count = node_count - zero_count;
     filter_vec(1:one_count) = 1;
-end
-
-function err = Get_MSE_Percent(signals, estimated_signals)
-    arguments
-        signals(:, :) double
-        estimated_signals(:, :) double {Must_Be_Equal_Size(signals, estimated_signals)}
-    end
-    rmse_ratio = norm(signals - estimated_signals, 'fro') / norm(signals, 'fro');
-    err = 100 * rmse_ratio^2;
 end

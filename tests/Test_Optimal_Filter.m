ee815316c4aa083638510c22d3f2f4c991150e6d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-23 21:58:19 +0300

    feat: add a simple test to run optimal filter
    Add a simple case for testing the optimal
    filter implementation, along with progressBar.

---
+++ tests/Test_Optimal_Filter.m
@@ -0,0 +1,93 @@
+% (c) Copyright 2023 Tuna Alikaşifoğlu
+
+%% Clear
+clc;
+clear;
+close all;
+
+%% Parameters
+rng(0);
+len = 100;
+noise_sigma = 0.1;
+threshold = 0.8;
+fractional_orders = 0:0.1:2;
+shift_mtx_strategy = 'adjacency';
+
+% Graph Generation
+A = randn(len, len);
+A = A + A';
+A = A > threshold;
+A = A - diag(diag(A));
+
+%% Graph Signal
+x = ones(len, 1);
+noise = noise_sigma * randn(size(x));
+x_noisy = x + noise;
+noisy_snr = Snr(x, x_noisy);
+
+Cxx = x * x';
+Cxn = zeros(size(Cxx));
+Cnx = Cxn';
+Cnn = noise_sigma^2 * eye(size(Cxx, 1));
+
+%% Experiment
+G = eye(length(x));
+if strcmp(shift_mtx_strategy, 'adjacency')
+    shift_mtx = A;
+    gft_mtx = GFT_Mtx(shift_mtx, 'tv');
+elseif strcmp(shift_mtx_strategy, 'laplacian')
+    shift_mtx = diag(sum(A, 2)) - A;
+    gft_mtx = GFT_Mtx(shift_mtx, 'ascend');
+end
+
+num_iterations = length(fractional_orders);
+snrs = zeros(num_iterations, 1);
+b = ProgressBar(num_iterations, ...
+                'IsParallel', true, ...
+                'WorkerDirectory', pwd(), ...
+                'Title', 'Optimal Filtering' ...
+               );
+b.setup([], [], []);
+parfor i = 1:num_iterations
+    [gfrft_mtx, igfrft_mtx] = GFRFT_Mtx(gft_mtx, fractional_orders(i));
+    x_filtered = Optimal_Filter_Known_Corr(G, gfrft_mtx, igfrft_mtx, ...
+                                           Cxx, Cxn, Cnx, Cnn, x_noisy);
+    snrs(i) = Snr(x, x_filtered);
+    updateParallel([], pwd);
+end
+b.release();
+ProgressBar.deleteAllTimers();
+
+%% Plot
+max_idx = Matrix_Idx(snrs, 'max');
+
+figure;
+legends = ['Filtered SNR', sprintf("Noisy SNR: %.2f", noisy_snr)];
+plot(fractional_orders, snrs, 'LineWidth', 2);
+hold on;
+yline(noisy_snr, '--', 'LineWidth', 2);
+
+max_point = plot(fractional_orders(max_idx), snrs(max_idx), '.');
+datatip(max_point);
+labels  = ["$a$", "SNR"];
+formats = ["%.2f", "%.2f"];
+for i = 1:length(max_point.DataTipTemplate.DataTipRows)
+    max_point.DataTipTemplate.DataTipRows(i).Label  = labels(i);
+    max_point.DataTipTemplate.DataTipRows(i).Format = formats(i);
+end
+max_point.DataTipTemplate.Interpreter = "latex";
+
+gft_idx = (fractional_orders == 1);
+if any(gft_idx) && (fractional_orders(max_idx) ~= 1)
+    gft_point = plot(fractional_orders(gft_idx), snrs(gft_idx), '.');
+    datatip(gft_point);
+    for i = 1:length(gft_point.DataTipTemplate.DataTipRows)
+        gft_point.DataTipTemplate.DataTipRows(i).Label  = labels(i);
+        gft_point.DataTipTemplate.DataTipRows(i).Format = formats(i);
+    end
+    gft_point.DataTipTemplate.Interpreter = "latex";
+end
+
+title('Fractional Order vs. SNRs');
+legend(legends);
+grid on;

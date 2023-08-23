35ac512df7a0f0045d4b29bf0c41592f1c27c56c
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-23 23:23:49 +0300

    fix(snr): remove `double` restriction
    Remove double restiriction from SNR calculation,
    since input signals can be complex.

--- src/Snr.m
+++ src/Snr.m
@@ -2,8 +2,8 @@
 
 function snr_db = Snr(signal, noisy_signal)
     arguments
-        signal double {mustBeNumeric}
-        noisy_signal double {Must_Be_Equal_Size(signal, noisy_signal)}
+        signal {mustBeNumeric}
+        noisy_signal {Must_Be_Equal_Size(signal, noisy_signal)}
     end
     snr_db = mag2db(rssq(signal(:)) / rssq(signal(:) - noisy_signal(:)));
 end

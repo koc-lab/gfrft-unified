81db167f90ababb4f91aa375384fe69311782b5e
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-27 01:21:26 +0300

    fix: allow matrix input to optimal filter
    In optimal filtering with know correlation,
    an optimal diagonal filter is obtained with
    given correlation matrices and the input
    is multiplied with this filter.
    
    Matrix input (joint time-vertex signal) can be
    allowed since it corresponds to finding an optimal
    diagonal filter with given matrices and applying
    it to every column individually. This is also
    what we want in the simplified optimal filtering
    task.
    
    Refs: #6

--- src/optimal-filter/Optimal_Filter_Known_Corr.m
+++ src/optimal-filter/Optimal_Filter_Known_Corr.m
@@ -14,8 +14,7 @@ function filtered_signal = Optimal_Filter_Known_Corr(transform_mtx, ...
         corr_xn(:, :) {mustBeNumeric, Must_Be_Equal_Size(corr_xn, corr_xx)}
         corr_nx(:, :) {mustBeNumeric, Must_Be_Equal_Size(corr_nx, corr_xx)}
         corr_nn(:, :) {mustBeNumeric, Must_Be_Equal_Size(corr_nn, corr_xx)}
-        graph_signal(:, 1) {mustBeNumeric, mustBeVector, ...
-                            Must_Be_Multiplicable(gfrft_mtx, graph_signal)}
+        graph_signal(:, :) {mustBeNumeric, Must_Be_Multiplicable(gfrft_mtx, graph_signal)}
         use_gpu(1, 1) logical {Must_Be_Logical} = false
     end
 
@@ -26,5 +25,6 @@ function filtered_signal = Optimal_Filter_Known_Corr(transform_mtx, ...
         h_opt = Get_Optimal_Filter(transform_mtx, gfrft_mtx, igfrft_mtx, ...
                                    corr_xx, corr_xn, corr_nx, corr_nn);
     end
+    h_opt = h_opt(:); % make sure column vector
     filtered_signal = igfrft_mtx * (h_opt .* (gfrft_mtx * graph_signal));
 end

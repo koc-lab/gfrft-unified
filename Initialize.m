9c5549f16f8f57590ef661b8a44ee6c7a7235dce
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-30 09:56:02 +0300

    feat: add `yaml` config reader as submodule
    Add matlab submodule `yaml` to read config files.

--- Initialize.m
+++ Initialize.m
@@ -10,5 +10,6 @@ addpath('gspbox');
 addpath('export_fig');
 addpath(genpath('src'));
 addpath(genpath('data'));
+addpath(genpath('yaml'));
 gsp_start;
 disp("Successfully initialized GFRFT Unified Project by Tuna Alikaşifoğlu.");

c3162f92df17ee1bdc0b6832b23e475e71b5d5ab
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-10-10 07:04:44 +0300

    fix(seed): tabular printing settings
    Update file path and remove artifacts.

--- optimal-filtering-task/seed/Print_Tabular_Seed.m
+++ optimal-filtering-task/seed/Print_Tabular_Seed.m
@@ -6,7 +6,8 @@ clear;
 close all;
 
 %% Load all mat files in into results cell array
-dataset_path = 'sst';
+dataset_path = 'synthetic-zero-mean-valid';
+filename = dataset_path;
 mat_files = dir(fullfile(dataset_path, '*.mat'));
 num_seeds = length(mat_files);
 results = cell(num_seeds, 1);
@@ -25,24 +26,12 @@ for iFile = 1:length(mat_files)
 end
 
 %%
-% i_result = 3;
-% for i_median = 1:length(results{i_result}.median_orders)
-%     fprintf("Median%d\n", results{i_result}.median_orders(i_median));
-%     for k_knn = 1:length(results{i_result}.knn_counts)
-%         for j_sigma = 1:length(results{i_result}.sigmas)
-%             fprintf("%.4f\n", results{i_result}.median_snrs(k_knn, j_sigma, i_median));
-%         end
-%     end
-% end
-
-%%
 [noisy_snrs_means, noisy_snrs_stds] = Get_Mean_Std_Pair(noisy_snrs_cell);
 [arma_snrs_means, arma_snrs_stds] = Get_Mean_Std_Pair(arma_snrs_cell);
 [median_snrs_means, median_snrs_stds] = Get_Mean_Std_Pair(median_snrs_cell);
 [gfrft_snrs_means, gfrft_snrs_stds] = Get_Mean_Std_Pair(gfrft_snrs_cell);
 
 %%
-filename = "sst";
 output_path = sprintf("%s.txt", filename);
 file_id = fopen(output_path, 'w');
 knn_counts = results{1}.knn_counts;

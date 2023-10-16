ca73bfe8cd23cb4fb48127385ab91f4ec24f61dd
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-10-16 14:23:38 +0300

    style(format): format with `miss_hit`
    Update the unformatted `.m` file.

--- optimal-filtering-task/comparison/Comparison.m
+++ optimal-filtering-task/comparison/Comparison.m
@@ -48,77 +48,77 @@ gfrft_snrs      = zeros(length(knn_counts), length(sigmas), ...
                         length(gfrft_strategies), length(fractional_orders));
 
 for seed = 0:10
-fprintf("Generating results for seed: %d\n", seed);
-for k_knn_count = 1:length(knn_counts)
-    knn_count = knn_counts(k_knn_count);
-    [graph, signals] = Init_KNN_Real(dataset, knn_count, knn_sigma, ...
-                                     max_node_count, max_time_instance, verbose);
-    rng(seed);
-    gpurng(seed);
-    % signals = signals / max(signals(:));
-    % signals = signals - mean(signals, 2);
-    signals = Normalize_Zero_One(signals);
-    % signals = Normalize_Plus_Minus_One(signals);
+    fprintf("Generating results for seed: %d\n", seed);
+    for k_knn_count = 1:length(knn_counts)
+        knn_count = knn_counts(k_knn_count);
+        [graph, signals] = Init_KNN_Real(dataset, knn_count, knn_sigma, ...
+                                         max_node_count, max_time_instance, verbose);
+        rng(seed);
+        gpurng(seed);
+        % signals = signals / max(signals(:));
+        % signals = signals - mean(signals, 2);
+        signals = Normalize_Zero_One(signals);
+        % signals = Normalize_Plus_Minus_One(signals);
 
-    % for i_sigma = 1:length(sigmas)
-    %     noisy_signals = signals + Generate_Noise(signals, sigmas(i_sigma));
-    %     noisy_snr = Snr(signals, noisy_signals);
-    %     fprintf("Noisy SNR: %.4f\n", noisy_snr);
-    % end
-    % return;
+        % for i_sigma = 1:length(sigmas)
+        %     noisy_signals = signals + Generate_Noise(signals, sigmas(i_sigma));
+        %     noisy_snr = Snr(signals, noisy_signals);
+        %     fprintf("Noisy SNR: %.4f\n", noisy_snr);
+        % end
+        % return;
 
-    fprintf("Generating results for %d-NN Graph...\n", knn_count);
-    for i_sigma = 1:length(sigmas)
-        %% Generate Noisy Signals
-        noisy_signals = signals + Generate_Noise(signals, sigmas(i_sigma));
-        noisy_snrs(k_knn_count, i_sigma) = Snr(signals, noisy_signals);
-        fprintf("Noisy SNR: %.4f\n", noisy_snrs(k_knn_count, i_sigma));
+        fprintf("Generating results for %d-NN Graph...\n", knn_count);
+        for i_sigma = 1:length(sigmas)
+            %% Generate Noisy Signals
+            noisy_signals = signals + Generate_Noise(signals, sigmas(i_sigma));
+            noisy_snrs(k_knn_count, i_sigma) = Snr(signals, noisy_signals);
+            fprintf("Noisy SNR: %.4f\n", noisy_snrs(k_knn_count, i_sigma));
 
-        %% ARMA Experiment
-        for j_arma = 1:length(arma_orders)
-            [arma_snr, lambda] = ARMA_Grid_Search(graph, signals, noisy_signals, ...
-                                                  arma_lambda_cuts, ...
-                                                  arma_orders(j_arma), arma_normalize);
-            arma_snrs(k_knn_count, i_sigma, j_arma) = arma_snr;
-            arma_lambdas(k_knn_count, i_sigma, j_arma) = lambda;
+            %% ARMA Experiment
+            for j_arma = 1:length(arma_orders)
+                [arma_snr, lambda] = ARMA_Grid_Search(graph, signals, noisy_signals, ...
+                                                      arma_lambda_cuts, ...
+                                                      arma_orders(j_arma), arma_normalize);
+                arma_snrs(k_knn_count, i_sigma, j_arma) = arma_snr;
+                arma_lambdas(k_knn_count, i_sigma, j_arma) = lambda;
+            end
+            fprintf("ARMA3: %.4f, ARMA4: %.4f, ARMA5: %.4f\n", ...
+                    arma_snrs(k_knn_count, i_sigma, 1), ...
+                    arma_snrs(k_knn_count, i_sigma, 2), ...
+                    arma_snrs(k_knn_count, i_sigma, 3));
+
+            %% Median Experiment
+            for j_median = 1:length(median_orders)
+                median_filtered_signals = Median_Filter(graph.A, noisy_signals, ...
+                                                        median_orders(j_median));
+                median_snrs(k_knn_count, i_sigma, j_median) = Snr(signals, median_filtered_signals);
+            end
+            fprintf("Median1: %.4f, Median2: %.4f\n", ...
+                    median_snrs(k_knn_count, i_sigma, 1), ...
+                    median_snrs(k_knn_count, i_sigma, 2));
+
+            %% GFRFT Experiment
+            for j_strategy = 1:length(gfrft_strategies)
+                strategy = gfrft_strategies(j_strategy);
+                [gft_mtx, igft_mtx, graph_freqs] = Get_GFT_With_Strategy(full(graph.W), strategy);
+                gfrft_transform_mtx = eye(size(signals, 1));
+
+                gfrft_snrs(k_knn_count, i_sigma, j_strategy, :) = ...
+                    GFRFT_Experiment(gft_mtx, gfrft_transform_mtx, signals, ...
+                                     noisy_signals, fractional_orders, uncorrelated);
+                [max_snr, max_idx] = max(gfrft_snrs(k_knn_count, i_sigma, j_strategy, :));
+                fprintf("Strategy: %s\n\tGFT: %.4f, GFRFT: %.4f (a=%.2f)\n", ...
+                        strategy, ...
+                        gfrft_snrs(k_knn_count, i_sigma, j_strategy, fractional_orders == 1), ...
+                        max_snr, fractional_orders(max_idx));
+            end
         end
-        fprintf("ARMA3: %.4f, ARMA4: %.4f, ARMA5: %.4f\n", ...
-                arma_snrs(k_knn_count, i_sigma, 1), ...
-                arma_snrs(k_knn_count, i_sigma, 2), ...
-                arma_snrs(k_knn_count, i_sigma, 3));
-
-        %% Median Experiment
-        for j_median = 1:length(median_orders)
-            median_filtered_signals = Median_Filter(graph.A, noisy_signals, ...
-                                                    median_orders(j_median));
-            median_snrs(k_knn_count, i_sigma, j_median) = Snr(signals, median_filtered_signals);
-        end
-        fprintf("Median1: %.4f, Median2: %.4f\n", ...
-                median_snrs(k_knn_count, i_sigma, 1), ...
-                median_snrs(k_knn_count, i_sigma, 2));
-
-        %% GFRFT Experiment
-        for j_strategy = 1:length(gfrft_strategies)
-            strategy = gfrft_strategies(j_strategy);
-            [gft_mtx, igft_mtx, graph_freqs] = Get_GFT_With_Strategy(full(graph.W), strategy);
-            gfrft_transform_mtx = eye(size(signals, 1));
-
-            gfrft_snrs(k_knn_count, i_sigma, j_strategy, :) = ...
-                GFRFT_Experiment(gft_mtx, gfrft_transform_mtx, signals, ...
-                                 noisy_signals, fractional_orders, uncorrelated);
-            [max_snr, max_idx] = max(gfrft_snrs(k_knn_count, i_sigma, j_strategy, :));
-            fprintf("Strategy: %s\n\tGFT: %.4f, GFRFT: %.4f (a=%.2f)\n", ...
-                    strategy, ...
-                    gfrft_snrs(k_knn_count, i_sigma, j_strategy, fractional_orders == 1), ...
-                    max_snr, fractional_orders(max_idx));
-        end
+        fprintf("\n\n\n");
     end
-    fprintf("\n\n\n");
-end
 
-%% Save Results
-filename = sprintf("comparison-%s-seed-%d.mat", dataset_title, seed);
-save(filename);
+    %% Save Results
+    filename = sprintf("comparison-%s-seed-%d.mat", dataset_title, seed);
+    save(filename);
 end
 
 %% Functions

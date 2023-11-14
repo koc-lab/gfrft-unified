edfb64d0a11318b953de83e55c035c822e8409d7
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-11-14 12:16:49 +0300

    fix(spectral): convert to `ones` in spectral
    The input signal was used as all ones, instead the
    spectral form is selected as all ones, so that
    an intuition can be provided.

--- spectral-modes/Main.m
+++ spectral-modes/Main.m
@@ -7,7 +7,7 @@ close all;
 
 %% Experiment Parameters
 vertex_count = 100;
-fractional_orders = 0:0.01:2;
+fractional_orders = 0:0.01:1;
 dataset_title = "sensor";
 gfrft_strategies = ["symmetric normalized adjacency", "normalized laplacian"];
 
@@ -21,13 +21,12 @@ else
 end
 
 graph = gsp_sensor(vertex_count);
-graph_signal = ones(vertex_count, 1);
-
 coefficents = zeros(length(gfrft_strategies), length(fractional_orders), vertex_count);
 graph_frequencies = zeros(length(gfrft_strategies), vertex_count);
 for j_strategy = 1:length(gfrft_strategies)
     strategy = gfrft_strategies(j_strategy);
     [gft_mtx, igft_mtx, graph_freqs] = Get_GFT_With_Strategy(full(graph.W), strategy);
+    graph_signal = gft_mtx * ones(vertex_count, 1);
     graph_frequencies(j_strategy, :) = graph_freqs;
     parfor i_order = 1:length(fractional_orders)
         order = fractional_orders(i_order);

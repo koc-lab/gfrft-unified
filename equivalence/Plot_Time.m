cd228df6b9a787e5b73ce9ed98157b20d02687f0
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-11-15 13:06:28 +0300

    feat(time): adjust time-frac plotting scheme
    Adjust the settings for time-frac plotting scheme.

--- equivalence/Plot_Time.m
+++ equivalence/Plot_Time.m
@@ -1,7 +1,8 @@
 % (c) Copyright 2023 Tuna Alikaşifoğlu
 
 function Plot_Time(power_durations, hyper_durations, ...
-                   fractional_orders, size_index, strategy_index)
+                   fractional_orders, sizes, strategy_index)
+    for size_index = 1:length(sizes)
     power_vals = squeeze(power_durations(size_index, strategy_index, :, :));
     hyper_vals = squeeze(hyper_durations(size_index, strategy_index, :, :));
 
@@ -10,12 +11,31 @@ function Plot_Time(power_durations, hyper_durations, ...
     hyper_mean = mean(hyper_vals, 2);
     hyper_std = std(hyper_vals, 0, 2);
 
-    figure;
+    fig = figure;
     bar(fractional_orders, 1000 * [power_mean, hyper_mean]);
-    title("Generation Duration Average of 20 Times");
+    max_val = max(max(power_mean), max(hyper_mean));
+    ylim([0, 1500 * max_val]);
     xlabel("Fractional Order");
-    ylabel("GFRFT Generation Duration from given GFT (ms)");
-    legend("Power", "Hyper");
-    xticks(fractional_orders);
+    ylabel("Duration (ms)");
+    legend(["Power", "Hyper"], 'Orientation','horizontal');
+    xticks(fractional_orders(1:2:end));
+    if size_index == 1
+        yticks([0, 25, 50]);
+    else
+        yticks([0, 100, 200]);
+    end
+    grid on;
+    grid minor;
 
+    set(gcf, 'Units', 'centimeters');
+    set(gcf, 'Position', [0, 0, 17.78, 3])
+    set(findall(fig,'-property','Box'),'Box','off') % optional
+    set(findall(fig, '-property', 'FontSize'), 'FontSize', 10);
+    set(findall(fig, '-property', 'Interpreter'), 'Interpreter', 'latex');
+    set(findall(fig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex');
+
+    ax = gca;
+    filename = sprintf('time_frac_n%d.eps', sizes(size_index));
+    exportgraphics(ax, filename, 'Resolution', 300);
+    end
 end

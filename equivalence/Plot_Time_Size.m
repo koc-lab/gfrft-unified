40510619ec22bc0ea509de523318b725c721f7ba
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-11-18 19:08:35 +0300

    fix(time): change plotting scheme
    Update time-size the plotting scheme to provide multiple
    GFT strategies on the same plot, but for one fractional order.

--- equivalence/Plot_Time_Size.m
+++ equivalence/Plot_Time_Size.m
@@ -2,46 +2,50 @@
 
 function Plot_Time_Size(power_durations, hyper_durations, ...
                         fractional_orders, sizes, gfrft_strategies)
-    marker_list = {'o', '^', 'd', 'v', '>', '<', 'p', 'h'};
+    marker_list = {'^', 'o', 'd', 'v', '>', '<', 'p', 'h'};
     marker_size = 4;
     line_width = 3;
 
-    for i_strategy = 1:length(gfrft_strategies)
-        power_vals = squeeze(power_durations(:, i_strategy, :, :));
-        hyper_vals = squeeze(hyper_durations(:, i_strategy, :, :));
-
+    for j_order = 1:length(fractional_orders)
         fig = figure;
         set(gcf, 'Units', 'centimeters');
-        set(gcf, 'Position', [0, 0, 17.78, 11]);
-
-        power_mean = mean(power_vals, ndims(power_vals));
-        power_std = std(power_vals, 0, ndims(power_vals));
-        power_upper = power_mean + power_std;
-        power_lower = power_mean - power_std;
-
-        hyper_mean = mean(hyper_vals, ndims(power_vals));
-        hyper_std = std(hyper_vals, 0, ndims(power_vals));
-        hyper_upper = hyper_mean + hyper_std;
-        hyper_lower = hyper_mean - hyper_std;
+        set(gcf, 'Position', [0, 0, 17.78, 13]);
 
         plts = [];
-        for j_order = 1:length(fractional_orders)
-            marker = marker_list{j_order};
-            plt = errorbar(sizes, power_mean(:, j_order), power_std(:, j_order), ...
+        for i_strategy = 1:length(gfrft_strategies)
+            power_vals = squeeze(power_durations(:, i_strategy, j_order, :));
+            power_mean = mean(power_vals, ndims(power_vals));
+            power_std = std(power_vals, 0, ndims(power_vals));
+            power_upper = power_mean + power_std;
+            power_lower = power_mean - power_std;
+            marker = marker_list{i_strategy};
+            strategy_name = char(gfrft_strategies(i_strategy));
+            short_name = [upper(strategy_name(1)), strategy_name(2:3), '.'];
+
+            plt = errorbar(sizes, power_mean, power_std, ...
                            'LineWidth', line_width, ...
                            'Marker', marker, ...
                            'MarkerSize', marker_size, ...
-                           'DisplayName', sprintf("Power, $a$ = %.2f", fractional_orders(j_order)));
+                           'DisplayName', sprintf("Power, %s", short_name));
             plts = [plts; plt];
             hold on;
         end
-        for j_order = 1:length(fractional_orders)
-            marker = marker_list{j_order};
-            plt = errorbar(sizes, hyper_mean(:, j_order), hyper_std(:, j_order), ...
+
+        for i_strategy = 1:length(gfrft_strategies)
+            hyper_vals = squeeze(hyper_durations(:, i_strategy, j_order, :));
+            hyper_mean = mean(hyper_vals, ndims(power_vals));
+            hyper_std = std(hyper_vals, 0, ndims(power_vals));
+            hyper_upper = hyper_mean + hyper_std;
+            hyper_lower = hyper_mean - hyper_std;
+            marker = marker_list{i_strategy};
+            strategy_name = char(gfrft_strategies(i_strategy));
+            short_name = [upper(strategy_name(1)), strategy_name(2:3), '.'];
+
+            plt = errorbar(sizes, hyper_mean, hyper_std, ...
                            'LineWidth', line_width, ...
                            'Marker', marker, ...
                            'MarkerSize', marker_size, ...
-                           'DisplayName', sprintf("Hyper, $a$ = %.2f", fractional_orders(j_order)));
+                           'DisplayName', sprintf("Hyper, %s", short_name));
             plts = [plts; plt];
             hold on;
         end
@@ -58,17 +62,13 @@ function Plot_Time_Size(power_durations, hyper_durations, ...
         ylim([y_min, y_max]);
         grid on;
         grid minor;
-        % for i = 1:8
-        %     fontsize("increase");
-        % end
-
         set(findall(fig, '-property', 'Box'), 'Box', 'off'); % optional
         set(findall(fig, '-property', 'FontSize'), 'FontSize', 25);
         set(findall(fig, '-property', 'Interpreter'), 'Interpreter', 'latex');
         set(findall(fig, '-property', 'TickLabelInterpreter'), 'TickLabelInterpreter', 'latex');
 
         ax = gca;
-        filename = sprintf('time_size_%s.eps', gfrft_strategies{i_strategy});
+        filename = sprintf('time_size_%.2f.eps', fractional_orders(j_order));
         exportgraphics(ax, filename, 'Resolution', 300);
     end
 end

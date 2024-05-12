e634a4a99e6d76b744905eb571ee46a72802f4c0
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-12 18:43:49 +0300

    fix(trainable): update trainable filter init
    The learnable diagonal graph filter weights were initialized
    through `torch.rand()`, this behavior is changed to have all
    ones, i.e., identity at the start. This allows better transform
    learning for both trainable transform and filter setting in
    the conceptual experiment.

--- trainable/filtering.py
+++ trainable/filtering.py
@@ -16,7 +16,7 @@ class TrainableDiagonalFilter(nn.Module):
         super().__init__()
         self.size = size
         self.device = None
-        self.filter = nn.Parameter(torch.randn(size))
+        self.filter = nn.Parameter(torch.ones(size))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.device is None:

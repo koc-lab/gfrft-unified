3458a98428a958b19478a604d91c51569eab862c
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-13 20:56:48 +0300

    feat: add heat distribution example

---
+++ spectral-modes/Heat.m
@@ -0,0 +1,46 @@
+% (c) Copyright 2023 Tuna Alikaşifoğlu
+
+%% Clear
+clc;
+clear;
+close all;
+
+%% Experiment Parameters
+index = 50;
+use_spectral_ones = false;
+vertex_count = 100;
+fractional_orders = 1:-0.25:0;
+dataset_title = "sensor";
+gfrft_strategies = ["adjacency", "laplacian"];
+
+%% Parallel Pool
+% pool = gcp('nocreate');
+% if isempty(pool)
+%     parpool();
+%     disp('Parallel pool created.');
+% else
+%     disp('Parallel pool already exists.');
+% end
+
+graph = gsp_sensor(vertex_count);
+for j_strategy = 1:length(gfrft_strategies)
+    strategy = gfrft_strategies(j_strategy);
+    [gft_mtx, igft_mtx, graph_freqs] = Get_GFT_With_Strategy(full(graph.W), strategy);
+    if use_spectral_ones
+        graph_signal = igft_mtx * ones(vertex_count, 1);
+    else
+        graph_signal = zeros(vertex_count, 1);
+        graph_signal(index) = 1;
+    end
+    for i_order = 1:length(fractional_orders)
+        order = fractional_orders(i_order);
+        [gfrft_mtx, igfrft_mtx] = GFRFT_Mtx(gft_mtx, order);
+        transformed = gfrft_mtx * graph_signal;
+        figure;
+        title(sprintf("%s, order %.2f", strategy, order));
+        plotparam.climits = [0, 1];
+        plotparam.vertex_size = 200;
+        gsp_plot_signal(graph, abs(transformed), plotparam);
+    end
+end
+ProgressBar.deleteAllTimers();
